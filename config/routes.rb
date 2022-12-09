3ce2e048699ddbe2fcf3cd94f4dcc2d14647b679
Sreyleak Deth <sreyleak.deth19@gmail.com>
2022-12-09 16:48:00 +0700

    Functional Nav Bar

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   get 'functional_nav_bar/login_signup'
   resources :invoices
 
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
